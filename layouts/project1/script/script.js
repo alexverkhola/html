0219f0883ff734e37b22ae00d78032290386223b
albatross86 <olexiy86@yandex.ua>
2017-02-28 20:14:52 +0200

    delete old files.

--- layouts/project1/script/script.js
+++
@@ -1,9 +0,0 @@
-function showtext(element) {
-    if(element.previousElementSibling.clientHeight === 80) {
-        element.previousElementSibling.style.height = '100%';
-        element.innerHTML = 'ShowLess...'
-    }else if(element.previousElementSibling.clientHeight > 80){
-        element.previousElementSibling.style.height = '80px';
-        element.innerHTML = 'Read More';
-    }
-}
